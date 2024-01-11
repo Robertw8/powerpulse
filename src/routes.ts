ab8d39e1b6c5ccf4a5e1493fec098850b0349d5d
Robertw8 <oniedgaf@gmail.com>
2024-01-11 05:55:53 +0200

    add routes examples

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+type Routes = { [key: string]: string };
+
+const routes: Routes = {
+  WELCOME: '/welcome',
+  SIGNUP: '/signup',
+  SIGNIN: '/signin',
+  PROFILE: '/profile',
+  DIARY: '/diary',
+  PRODUCTS: '/products',
+  EXERCISES: '/exercises',
+};
+
+export default routes;
