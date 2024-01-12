098fd3c9e7278494d220b48f52fead7dbd516268
Robertw8 <oniedgaf@gmail.com>
2024-01-12 02:35:24 +0200

    filter dropdown demo

---
+++ src/components/Products/FilterDropdown/FilterDropdown.styled.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+const Dropdown = styled.div`
+  position: relative;
+
+  width: 146px;
+  height: 46px;
+  padding: 14px;
+  border: 1px solid #efede84d;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  font-size: 14px;
+  border-radius: 12px;
+  transition-duration: 150ms;
+
+  &:is(:hover, :focus) {
+    cursor: pointer;
+    border-color: #e6533c;
+  }
+`;
+
+const DropdownList = styled.ul`
+  position: absolute;
+  top: 80%;
+  left: 0;
+
+  width: 100%;
+  padding: 14px 28px 14px 14px;
+
+  background-color: #1c1c1c;
+  border-radius: 12px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  transition-duration: 150ms;
+  transition-timing-function: ease-in;
+  transform-origin: top;
+
+  transform: scaleY(${({ visible }) => (visible ? 1 : 0)});
+`;
+
+const DropdownItem = styled.li`
+  &:not(:first-of-type) {
+    margin-top: 8px;
+  }
+`;
+
+export { Dropdown, DropdownList, DropdownItem };
