b33394b688e14f4879c77da2ca52bcb583797a46
Robertw8 <oniedgaf@gmail.com>
2024-01-12 22:02:28 +0200

    add ...rest props for InputPrimary

--- src/components/InputPrimary/InputPrimary.tsx
+++ src/components/InputPrimary/InputPrimary.tsx
@@ -3,14 +3,21 @@ import { StyledInput } from './InputPrimary.styled';
 interface InputPrimaryProps {
   placeholder: string;
   bordercolor: 'default' | 'success' | 'error';
+  [key: string]: unknown;
 }
 
 const InputPrimary: React.FC<InputPrimaryProps> = ({
   placeholder,
-
   bordercolor,
+  ...rest
 }) => {
-  return <StyledInput placeholder={placeholder} bordercolor={bordercolor} />;
+  return (
+    <StyledInput
+      placeholder={placeholder}
+      bordercolor={bordercolor}
+      {...rest}
+    />
+  );
 };
 
 export default InputPrimary;
