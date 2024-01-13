9d263b867a3ba1d5fa15c66dbd377545ae186c44
Robertw8 <oniedgaf@gmail.com>
2024-01-13 02:47:38 +0200

    create ProductsItem

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import { Layout } from './Layout';
 import { Header } from './Header';
 import { PrimaryButton } from './PrimaryButton';
 import { Container } from './Container';
+import { ProductsItem } from './Products';
 
-export { App, Layout, Header, PrimaryButton, Container };
+export { App, Layout, Header, PrimaryButton, Container, ProductsItem };
