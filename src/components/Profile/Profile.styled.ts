471010b3746440789c018f77814bfd827d8a2345
gladja <gladja@MacBook-Pro-gladja.local>
2024-01-14 11:03:49 +0200

    PageTitle add margin, sprite del width and height,Profile component adaptive

--- src/components/Profile/Profile.styled.ts
+++ src/components/Profile/Profile.styled.ts
@@ -1,108 +1,183 @@
 import styled from '@emotion/styled';
-import {  Button  } from 'antd';
+import { Button } from 'antd';
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-`
+`;
 
-const Title = styled.h2`
-margin: 40px 0;
-// text-align: center;
-`
+// const Title = styled.h1`
+//   margin: 40px 0;
+//   font-size: 24px;
+//   font-weight: 700;
+//   line-height: 28px;
+
+//   @media screen and (min-width: 769px) {
+//     font-size: 32px;
+//     line-height: 44px;
+//   }
+
+//   @media screen and (min-width: 1441px) {
+//   }
+// `;
 
 const ImgWrap = styled.div`
-width: 90px;
-height: 90px;
-border: 2px solid #E6533C;
-border-radius: 50%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin: 0 auto ;
-padding: 6px 0 0
-`
+  width: 90px;
+  height: 90px;
+  border: 2px solid #e6533c;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto 30px;
+
+  @media screen and (min-width: 769px) {
+    width: 150px;
+    height: 150px;
+    margin: 22px auto 30px;
+  }
+`;
+
+const ImgUserAvatar = styled.div`
+  position: relative;
+  top: 15px;
+`;
 
 const UserAvatar = styled.div`
-position: relative;
-top: 15px;
-` 
+  position: relative;
+  top: 10px;
+
+  @media screen and (min-width: 769px) {
+    top: 22px;
+  }
+`;
 
 const BtnAvatar = styled.button`
- padding: 0;
+  padding: 0;
   border: none;
   background-color: transparent;
+`;
 
-`
+const UserWrap = styled.div`
+  text-align: center;
+`;
+const UserName = styled.h3`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  margin: 0 0 4px;
+
+  @media screen and (min-width: 769px) {
+    font-size: 24px;
+    line-height: 28px;
+  }
+`;
+const UserStatus = styled.p`
+  color: rgba(239, 237, 232, 0.3);
+  font-size: 14px;
+  line-height: 18px;
+`;
 
 const BlockWrap = styled.div`
-margin: 40px 0 ;
-display: flex;
-justify-content: center;
-gap: 14px;
-`
+  margin: 40px 0;
+  display: flex;
+  justify-content: center;
+  gap: 14px;
+`;
 const BlockData = styled.div`
-width: 157px;
-height: 96px;
-padding: 13px;
-border-radius: 12px;
-border: 1px solid rgba(239, 237, 232, 0.20);
-background: #E6533C;
+  width: 157px;
+  height: 96px;
+  padding: 13px;
+  border-radius: 12px;
+  border: 1px solid rgba(239, 237, 232, 0.2);
+  background: #e6533c;
 
-display: flex;
-justify-content: space-between;
-flex-direction: column;
-`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  @media screen and (min-width: 769px) {
+    width: 214px;
+    height: 108px;
+    padding: 14px 18px;
+  }
+`;
 
 const TextBlockWrap = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
-gap: 8px;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+`;
 
 const TitleBlock = styled.p`
-font-size: 12px;
-color: rgba(239, 237, 232, 0.80);
-line-height: 16px;
-`
+  font-size: 12px;
+  color: rgba(239, 237, 232, 0.8);
+  line-height: 16px;
+`;
 
 const TextWrap = styled.div`
-display: flex;
-gap: 8px;
-justify-content: center;
-`
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+`;
 
 const TextValue = styled.p`
-font-size: 18px;
-font-weight: 700;
-line-height: 20px;
-`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 20px;
+
+  @media screen and (min-width: 769px) {
+    font-size: 32px;
+  }
+`;
 
 const Text = styled.p`
-width: 310px;
-color: rgba(239, 237, 232, 0.30);
-font-size: 14px;
-font-style: normal;
-line-height: 18px; 
-`
+  width: 310px;
+  color: rgba(239, 237, 232, 0.3);
+  font-size: 14px;
+  line-height: 18px;
+
+  @media screen and (min-width: 769px) {
+    font-size: 16px;
+    width: 410px;
+  }
+`;
 
 const Btn = styled(Button)`
-width: 115px;
-height: 42px;
-border: none;
-background-color: #e6533c;
-color: #EFEDE8;
-// margin-top: 64px;
-// padding: 12px 40px;
+  width: 115px;
+  height: 42px;
+  border: none;
+  background-color: #e6533c;
+  color: #efede8;
+  // margin-top: 64px;
+  // padding: 12px 40px;
 
   &:hover,
   &:focus {
-    background-color:#EF8964;
+    background-color: #ef8964;
   }
-`
+`;
 
-export { Btn, Title, ImgWrap, Text, TextWrap, BlockWrap, BlockData, Wrap, TitleBlock, TextBlockWrap, UserAvatar, BtnAvatar, TextValue }
+export {
+  Btn,
+  ImgWrap,
+  Text,
+  TextWrap,
+  BlockWrap,
+  BlockData,
+  Wrap,
+  TitleBlock,
+  TextBlockWrap,
+  UserAvatar,
+  BtnAvatar,
+  TextValue,
+  UserName,
+  UserWrap,
+  UserStatus,
+  ImgUserAvatar,
+};
