7f855533d8fb0dc2550de24c483a23bd1e3c19ee
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2024-01-14 16:32:36 +0200

    update sprite.svg, update header, update headerStyled, add MobileMenuBtn

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+import { Link } from 'react-router-dom';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px 20px;
+  margin: 0 auto;
+
+  background-color: var('--black-color');
+
+  @media screen and (min-width: 768px) {
+    padding: 32px 32px;
+  }
+  @media screen and (min-width: 1440px) {
+    padding: 32px 96px;
+    width: 1440px;
+  }
+`;
+export const LinkLogo = styled(Link)`
+  margin-right: auto;
+`;
+
+export const UserContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  background-color: black;
+`;
