b71acc6e314122bc8fd44d1ae00cc84da7a190f4
Robertw8 <oniedgaf@gmail.com>
2024-01-16 22:50:06 +0200

    replace error handler from operationWrapper to apiService

--- src/helpers/operationWrapper.ts
+++ src/helpers/operationWrapper.ts
@@ -2,15 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const operationWrapper = <Returned, ThunkArg>(
   path: string,
-  handler: (args: ThunkArg, thunkAPI) => Promise<Returned>,
-  errorHandler = () => 'An error occurred'
+  handler: (args: ThunkArg, thunkAPI) => Promise<Returned>
 ) => {
   return createAsyncThunk<Returned, ThunkArg>(path, async (args, thunkAPI) => {
     try {
       return await handler(args, thunkAPI);
     } catch (error) {
-      if (errorHandler) return thunkAPI.rejectWithValue(errorHandler());
-
       return thunkAPI.rejectWithValue(
         error instanceof Error ? error.message : 'An error occurred'
       );
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,7 +3,10 @@ import { ApiServiceOptions } from './types';
 
 axios.defaults.baseURL = 'https://goit-be.onrender.com/';
 
-const apiService = async ({ method, url, data, config }: ApiServiceOptions) => {
+const apiService = async (
+  { method, url, data, config }: ApiServiceOptions,
+  errorHandler = () => 'An error occurred'
+) => {
   try {
     const response = await axios.request({
       method,
@@ -14,7 +17,7 @@ const apiService = async ({ method, url, data, config }: ApiServiceOptions) => {
 
     return response.data;
   } catch (error) {
-    return error instanceof Error ? error.message : 'An error occurred';
+    return errorHandler();
   }
 };
 
