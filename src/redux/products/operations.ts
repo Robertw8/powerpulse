c03ffb0fe14eb9b8ceea8b44d356b67cdd855435
Robertw8 <oniedgaf@gmail.com>
2024-01-16 03:28:22 +0200

    fix redux ts error

--- src/redux/products/operations.ts
+++ src/redux/products/operations.ts
@@ -1,13 +1,10 @@
 import { operationWrapper } from '../../helpers';
 import { apiService } from '../../services';
 
-const getProducts = operationWrapper({
-  path: 'products/getProducts',
-  handler: async () => {
-    const response = await apiService({ method: 'get', url: 'products' });
+const getProducts = operationWrapper('products/getProducts', async () => {
+  const response = await apiService({ method: 'get', url: 'products' });
 
-    return response.data;
-  },
+  return response.data;
 });
 
 export { getProducts };
