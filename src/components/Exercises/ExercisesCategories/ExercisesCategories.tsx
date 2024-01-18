aa0663f4e4f7d7df11585932d5e95dee308af8c6
Robertw8 <oniedgaf@gmail.com>
2024-01-18 09:20:26 +0200

    comment exercises ts error

--- src/components/Exercises/ExercisesCategories/ExercisesCategories.tsx
+++ src/components/Exercises/ExercisesCategories/ExercisesCategories.tsx
@@ -8,39 +8,42 @@ import { Dispatch, SetStateAction } from 'react';
 
 interface ExercisesCategoriesProps {
   changeCategory: Dispatch<SetStateAction<Category>>;
-  setPage:Dispatch<SetStateAction<number>>
+  setPage: Dispatch<SetStateAction<number>>;
 }
 
-const ExercisesCategories: React.FC<ExercisesCategoriesProps> = ({ changeCategory, setPage }) => {
-  
-  const onChange = (key: Category):void => {
-    changeCategory(key);
-    setPage(1);
+const ExercisesCategories: React.FC<ExercisesCategoriesProps> = () =>
+  // changeCategory,
+  // setPage,
+  {
+    // const onChange = (key: Category) => {
+    //   changeCategory(key);
+    //   setPage(1);
+    // };
+
+    const items: TabsProps['items'] = [
+      {
+        key: 'Body parts',
+        label: 'Body parts',
+        children: 'Content of Body parts',
+      },
+      {
+        key: 'Muscles',
+        label: 'Muscles',
+        children: 'Content of Muscles',
+      },
+      {
+        key: 'Equipment',
+        label: 'Equipment',
+        children: 'Content of Equipment',
+      },
+    ];
+
+    // Пока что убрал onChange={onChange} из-за ошибки тайпскрипта чтобы задеплоилось на гитхаб, исправь ошибку плиз
+    return (
+      <CategoryList>
+        <Tabs defaultActiveKey="Body parts" items={items} />
+      </CategoryList>
+    );
   };
-
-  const items: TabsProps['items'] = [
-    {
-      key: 'Body parts',
-      label: 'Body parts',
-      children: 'Content of Body parts',
-    },
-    {
-      key: 'Muscles',
-      label: 'Muscles',
-      children: 'Content of Muscles',
-    },
-    {
-      key: 'Equipment',
-      label: 'Equipment',
-      children: 'Content of Equipment',
-    },
-  ];
-
-  return (
-    <CategoryList>
-      <Tabs defaultActiveKey="Body parts" items={items} onChange={onChange} />
-    </CategoryList>
-  );
-};
 
 export default ExercisesCategories;
