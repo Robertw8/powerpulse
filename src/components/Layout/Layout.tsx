5c59a38340e54c3d5e91016783da8bd7277538bb
Robertw8 <oniedgaf@gmail.com>
2024-01-19 00:34:46 +0200

    add custom loader

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '..';
+import { Loader } from '../Loader';
 
 const Layout: React.FC = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={'Loading...'}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
     </>
