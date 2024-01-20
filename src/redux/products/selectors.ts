e9a7a80c62433d52fe7b680287750a289dfdab03
Robertw8 <oniedgaf@gmail.com>
2024-01-20 08:54:59 +0200

    add calories and categories actions

--- src/redux/products/selectors.ts
+++ src/redux/products/selectors.ts
@@ -2,3 +2,6 @@ import { RootState } from '../rootReducer';
 
 export const selectProducts = (state: RootState) => state.products.products;
 export const selectIsLoading = (state: RootState) => state.products.isLoading;
+export const selectCalculatedCalories = (state: RootState) =>
+  state.products.calculatedCalories;
+export const selectCategories = (state: RootState) => state.products.categories;
