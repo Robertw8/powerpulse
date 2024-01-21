f299713d2c58ddf4720c34a49dd55eb986ff91fd
AntonZinchenko32 <zinchenko309@gmail.com>
2024-01-21 14:41:51 +0200

    export StatisticsReducer fix

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,7 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { persistedAuthReducer } from './auth';
 import { productsReducer } from './products';
 import { exercisesReducer } from './Waist';
-import { statisticsReducer } from './Waist';
+import { statisticsReducer } from './statistics';
 
 const rootReducer = combineReducers({
   auth: persistedAuthReducer,
--- src/redux/statistics/index.ts
+++ src/redux/statistics/index.ts
@@ -1,3 +1,4 @@
 export { getStatistics } from './operations';
 
 export { selectStatistics } from './selectors';
+export { statisticsReducer } from './slice';
