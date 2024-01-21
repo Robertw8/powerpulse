f9abc61517dc726f66e9dd88e2fcad60ad6a3a6b
Robertw8 <oniedgaf@gmail.com>
2024-01-22 01:18:48 +0200

    implement redux for adding products to diary

---
+++ src/helpers/getCurrentDate.ts
@@ -0,0 +1,10 @@
+const getCurrentDate = () => {
+  const now = new Date();
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const year = now.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+export default getCurrentDate;
--- src/redux/diary/operations.ts
+++ src/redux/diary/operations.ts
@@ -1,89 +1,85 @@
-
 import { operationWrapper } from '../../helpers';
-  import { apiService} from '../../services';
+import { apiService } from '../../services';
 
-const getDiary = operationWrapper(
-    'diary/getDiary',
-    async (date) => {
- 
-      const response = await apiService({
-        method: 'get',
-        url: 'diary',
-config:{params:{date}}
-    
-    
-      });
-     
-  console.log(response);
-      return response
-    }
-  );
- 
-  const addDiaryProduct = operationWrapper(
-    'diary/addDiaryProduct',
-    async (data) => {
- 
-      const response = await apiService({
-        method: 'post',
-        url: 'diary/products',
-        data
-    
-    
-      });
-     
-  console.log(response);
-      return response.data;
-    }
-  );
+const getDiary = operationWrapper('diary/getDiary', async date => {
+  const response = await apiService({
+    method: 'get',
+    url: 'diary',
+    config: { params: { date } },
+  });
 
-  const deleteDiaryProduct = operationWrapper(
-    'diary/deleteDiaryProduct',
-    async (productId) => {
- 
-      const response = await apiService({
-        method: 'delete',
-        url: `diary/products/${productId}`,
-       
-    
-    
-      });
-     
   console.log(response);
-      return response;
-    }
-  );
+  return response;
+});
 
-  const addDiaryExercise = operationWrapper(
-    'diary/addDiaryExercise',
-    async (data) => {
- 
-      const response = await apiService({
-        method: 'post',
-        url: 'diary/exercises',
-        data
-    
-    
-      });
-     
-  console.log(response);
-      return response.data;
-    }
-  );
+interface AddDiaryProduct {
+  id: string;
+  date: string;
+  amount: number;
+  calories: number;
+}
 
-  const deleteDiaryExercise  = operationWrapper(
-    'diary/deleteDiaryExercise',
-    async (exerciseId) => {
- 
-      const response = await apiService({
-        method: 'delete',
-        url: `diary/exercises/${exerciseId}`,
-       
-    
-    
-      });
-     
-  console.log(response);
-      return response;
-    }
-  );
-  export { getDiary,addDiaryProduct,deleteDiaryProduct,addDiaryExercise,deleteDiaryExercise }
+const addDiaryProduct = operationWrapper(
+  'diary/addDiaryProduct',
+  async ({ id, date, amount, calories }: AddDiaryProduct) => {
+    const response = await apiService({
+      method: 'post',
+      url: 'diary/products',
+      data: {
+        product_ID: id,
+        date,
+        amount,
+        calories,
+      },
+    });
+
+    return response;
+  }
+);
+
+const deleteDiaryProduct = operationWrapper(
+  'diary/deleteDiaryProduct',
+  async productId => {
+    const response = await apiService({
+      method: 'delete',
+      url: `diary/products/${productId}`,
+    });
+
+    console.log(response);
+    return response;
+  }
+);
+
+const addDiaryExercise = operationWrapper(
+  'diary/addDiaryExercise',
+  async data => {
+    const response = await apiService({
+      method: 'post',
+      url: 'diary/exercises',
+      data,
+    });
+
+    console.log(response);
+    return response.data;
+  }
+);
+
+const deleteDiaryExercise = operationWrapper(
+  'diary/deleteDiaryExercise',
+  async exerciseId => {
+    const response = await apiService({
+      method: 'delete',
+      url: `diary/exercises/${exerciseId}`,
+    });
+
+    console.log(response);
+    return response;
+  }
+);
+export {
+  getDiary,
+  addDiaryProduct,
+  deleteDiaryProduct,
+  addDiaryExercise,
+  deleteDiaryExercise,
+};
