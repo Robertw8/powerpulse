87d7a426b4f0cf6b499a8e0b5dbb7f7b1bf2d43c
AntonZinchenko32 <zinchenko309@gmail.com>
2024-01-22 14:38:34 +0200

    created Statistics folder for Statistics component

---
+++ src/components/Statistics/Statistics.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Col, Row, Statistic, Divider } from 'antd';
+
+const Statistics: React.FC = () => {};
+export default Statistics;
