8a42bcee5000d1b1931e01debea4a256bd15770d
Robertw8 <oniedgaf@gmail.com>
2024-01-22 02:12:17 +0200

    add useDiary custom hook

---
+++ src/hooks/useDiary.ts
@@ -0,0 +1,38 @@
+import { useSelector } from 'react-redux';
+import {
+  selectProducts,
+  selectExercises,
+  selectCaloriesBurned,
+  selectCaloriesConsumed,
+  selectCaloriesRemaining,
+  selectSportsTime,
+  selectSportsRemaining,
+  selectIsLoading,
+  selectError,
+} from '../redux/diary';
+
+const useDiary = () => {
+  const diaryProducts = useSelector(selectProducts);
+  const diaryExercises = useSelector(selectExercises);
+  const caloriesBurned = useSelector(selectCaloriesBurned);
+  const caloriesConsumed = useSelector(selectCaloriesConsumed);
+  const caloriesRemaining = useSelector(selectCaloriesRemaining);
+  const sportsTime = useSelector(selectSportsTime);
+  const sportsRemaining = useSelector(selectSportsRemaining);
+  const isLoading = useSelector(selectIsLoading);
+  const diaryError = useSelector(selectError);
+
+  return {
+    diaryProducts,
+    diaryExercises,
+    caloriesBurned,
+    caloriesConsumed,
+    caloriesRemaining,
+    sportsTime,
+    sportsRemaining,
+    isLoading,
+    diaryError,
+  };
+};
+
+export default useDiary;
--- src/redux/diary/index.ts
+++ src/redux/diary/index.ts
@@ -1,6 +1,19 @@
-export  { getDiary,
-    addDiaryProduct,deleteDiaryProduct,
-    addDiaryExercise,deleteDiaryExercise
-} from "./operations";
+export {
+  getDiary,
+  addDiaryProduct,
+  deleteDiaryProduct,
+  addDiaryExercise,
+  deleteDiaryExercise,
+} from './operations';
 export { diaryReducer } from './slice';
-
+export {
+  selectProducts,
+  selectExercises,
+  selectCaloriesBurned,
+  selectCaloriesConsumed,
+  selectCaloriesRemaining,
+  selectSportsTime,
+  selectSportsRemaining,
+  selectIsLoading,
+  selectError,
+} from './selectors';
