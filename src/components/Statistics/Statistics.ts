d8945abb6bcb45f7a66126a9c1d556fe36345f68
AntonZinchenko32 <zinchenko309@gmail.com>
2024-01-22 11:38:32 +0200

    tries to add the antd component

---
+++ src/components/Statistics/Statistics.ts
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Col, Row, Statistic } from 'antd';
+
+const Statistic: React.FC = () => {
+  return (
+    <Row gutter={16}>
+      <Col span={12}>
+        <Card bordered={false}>
+          <Statistic
+            title="Active"
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<ArrowUpOutlined />}
+            suffix="%"
+          />
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card bordered={false}>
+          <Statistic
+            title="Idle"
+            value={9.3}
+            precision={2}
+            valueStyle={{ color: '#cf1322' }}
+            prefix={<ArrowDownOutlined />}
+            suffix="%"
+          />
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default Statistic;
