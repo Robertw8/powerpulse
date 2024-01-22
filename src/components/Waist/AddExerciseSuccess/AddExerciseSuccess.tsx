4874cf646a228212eea536c555d24f4f3858a069
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2024-01-22 15:38:21 +0200

    commit

--- src/components/Waist/AddExerciseSuccess/AddExerciseSuccess.tsx
+++ src/components/Waist/AddExerciseSuccess/AddExerciseSuccess.tsx
@@ -1,1 +1,85 @@
-
+import { useEffect } from 'react'; 
+import { Transition } from 'react-transition-group'; 
+import { 
+  Modal, 
+  MadalContent, 
+  Image, 
+  Span, 
+  P, 
+  Button, 
+  Div,  
+  Svg, 
+  H2, 
+  Button2, 
+} from './AddExerciseSuccess.styled'; 
+import sprite from '../../../assets/images/sprite.svg'; 
+ 
+interface AddExerciseSuccessProps { 
+  isOpen: boolean; 
+  onClose: () => void; 
+  calories: number; 
+} 
+ 
+const AddExerciseSuccess: React.FC<AddExerciseSuccessProps> = ({ isOpen, onClose, calories }) => { 
+  const onWrapperClick = (event: React.MouseEvent<HTMLDivElement>) => { 
+    if ((event.target as HTMLElement).classList.contains('modal-wrapper')) onClose(); 
+  }; 
+ 
+  useEffect(() => { 
+    const onKeydownEsc = (e: KeyboardEvent) => { 
+      if (e.code === 'Escape') onClose(); 
+    }; 
+    window.addEventListener('keydown', onKeydownEsc); 
+    return () => { 
+      window.removeEventListener('keydown', onKeydownEsc); 
+    }; 
+  }, [onClose]); 
+ 
+  return ( 
+    <> 
+      <Transition in={isOpen} timeout={350} unmountOnExit={true}> 
+        <Modal className="modal-wrapper" onClick={onWrapperClick}> 
+          <MadalContent className="modal-content"> 
+            <Svg onClick={() => onClose()}> 
+              <use href={`${sprite}#icon-x`}></use> 
+            </Svg> 
+            <Div> 
+              <Image></Image> 
+              <H2>Well done</H2> 
+              <P> 
+                Your time:<Span> 3 minutes</Span> 
+              </P> 
+              <P> 
+                Burned calories: <Span>{calories}</Span> 
+              </P> 
+            </Div> 
+            <Button type="button"> 
+              Next product 
+            </Button> 
+            <Button2 type="button"> 
+              To the diary 
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="16" 
+                height="16" 
+                viewBox="0 0 16 16" 
+                fill="none" 
+              > 
+                <path 
+                  d="M7.5 14L14 7.5M14 7.5L7.5 1M14 7.5H1" 
+                  stroke="#EFEDE8" 
+                  strokeOpacity="0.3" 
+                  strokeWidth="1.3" 
+                  strokeLinecap="round" 
+                  strokeLinejoin="round" 
+                /> 
+              </svg> 
+            </Button2> 
+          </MadalContent> 
+        </Modal> 
+      </Transition> 
+    </> 
+  ); 
+}; 
+ 
+export default AddExerciseSuccess;
--- src/components/Waist/BasicModalWindow/BasicModalWindow.tsx
+++ src/components/Waist/BasicModalWindow/BasicModalWindow.tsx
@@ -1,150 +1,170 @@
-import React, { useEffect, useState } from 'react';
-import { Transition } from 'react-transition-group';
-
-import {
-  Modal,
-  MadalContent,
-  Image,
-  Timer,
-  BurnedCal,
-  BurnedInformName,
-  Ul,
-  Li,
-  Span,
-  P,
-  Button,
-  Div,
-  SvgPlay,
-  SvgPause,
-  Btn,
-  Svg,
-} from './BasicModalWindow.styled';
-
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
-
-import sprite from '../../../assets/images/sprite.svg';
-
-let time = 10;
-
-const BasicModalWindow: React.FC<{
-  isOpen: boolean;
-  onClose: () => void;
-  Burned: number;
-  Body: string;
-  Target: string;
-}> = ({ isOpen, onClose, Burned, Body, Target }) => {
-  const [pause, setPause] = useState(true);
-  const [burned, setBurned] = useState(0);
-
-  const onWrapperClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if (event.target.classList.contains('modal-wrapper')) onClose();
-  };
-
-  useEffect(() => {
-    let intervalId: NodeJS.Timeout | null = null;
-
-    if (isOpen && pause && burned < Burned) {
-      if (burned > Burned) {
-        clearInterval(intervalId as NodeJS.Timeout);
-        return;
-      }
-      intervalId = setInterval(() => {
-        setBurned(prevBurned => {
-          const coloriy = (time * Burned) / 180;
-          const roundedNumber = Math.round(coloriy);
-          return roundedNumber;
-        });
-        time += 10;
-      }, 10000);
-    }
-
-    return () => clearInterval(intervalId as NodeJS.Timeout);
-  }, [isOpen, pause, burned, setBurned]);
-
+import React, { useEffect, useState } from 'react'; 
+import { Transition } from 'react-transition-group'; 
+import {  
+  Modale, 
+  MadalContent, 
+  Image, 
+  Timer, 
+  BurnedCal, 
+  BurnedInformName, 
+  Ul, 
+  Li, 
+  Span, 
+  P, 
+  Button, 
+  Div, 
+  SvgPlay, 
+  SvgPause, 
+  Btn, 
+  Svg, 
+} from './BasicModalWindow.styled'; 
+import sprite from '../../assets/images/sprite.svg'; 
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'; 
+ 
+interface BasicModalWindowProps { 
+  isOpen: boolean; 
+  onClose: () => void; 
+  onOpen: () => void; 
+  Burned: number; 
+  Body: string; 
+  Target: string; 
+  burned: number; 
+  setBurned: React.Dispatch<React.SetStateAction<number>>; 
+} 
+ 
+let time = 10; 
+ 
+const BasicModalWindow: React.FC<BasicModalWindowProps> = ({ 
+  isOpen, 
+  onClose, 
+  onOpen, 
+  Burned, 
+  Body, 
+  Target, 
+  burned, 
+  setBurned, 
+}) => { 
+  const [pause, setPause] = useState(true); 
+ 
+  const onWrapperClick = (event: React.MouseEvent<HTMLDivElement>) => { 
+    if ((event.target as HTMLElement).classList.contains('modal-wrapper')) onClose(); 
+  }; 
+ 
   useEffect(() => { 
-    const onKeydownEsc = e => { 
+    let intervalId: NodeJS.Timeout | null = null; 
+ 
+    if (isOpen && pause && burned < Burned) { 
+      intervalId = setInterval(() => { 
+        setBurned((prevBurned) => { 
+          const coloriy = (time * Burned) / 180; 
+          const roundedNumber = Math.round(coloriy); 
+          return roundedNumber; 
+        }); 
+        time += 10; 
+      }, 10000); 
+    } 
+ 
+    return () => clearInterval(intervalId!); 
+  }, [isOpen, pause, burned, setBurned, Burned]); 
+ 
+  useEffect(() => { 
+    const onKeydownEsc = (e: KeyboardEvent) => { 
       if (e.code === 'Escape') onClose(); 
     }; 
     window.addEventListener('keydown', onKeydownEsc); 
     return () => { 
       window.removeEventListener('keydown', onKeydownEsc); 
     }; 
-  }, [onClose]);
-
-  const startPauseTimer = () => {
-    setPause(prevPause => !prevPause);
-  };
-
-  const namber = 180;
-
-  return (
-    <>
-      <Transition in={isOpen} timeout={350} unmountOnExit={true}>
-          <Modal
-            className='modal-wrapper'
-            onClick={onWrapperClick}
-          >
-              <MadalContent className="modal-content">
-                <Svg onClick={() => onClose()}>
-                  <use href={`${sprite}#icon-x`}></use>
-                </Svg>
-                <Div>
-                  <Image></Image>
-                  <Timer>
-                    <CountdownCircleTimer
-                      isPlaying={pause}
-                      duration={namber}
-                      colors={'#E6533C'}
-                      colorsTime={0}
-                      size={124}
-                      strokeWidth={4}
-                      trailColor={'rgba(239, 237, 232, 0.10)'}
-                      isGrowing={true}
-                      rotation={'counterclockwise'}
-                      strokeLinecap={'square'}
-                      children={({ remainingTime }) => (
-                        <div className="countdown-timer">
-                          {`${Math.floor(remainingTime / 60)}:${
-                            remainingTime % 60
-                          }`}
-                        </div>
-                      )}
-                    ></CountdownCircleTimer>
-                    <Btn type="button" onClick={startPauseTimer}>
-                      {pause ? (
-                        <SvgPlay>
-                          <use href={`${sprite}#icon-play`}></use>
-                        </SvgPlay>
-                      ) : (
-                        <SvgPause>
-                          <use href={`${sprite}#icon-pause`}></use>
-                        </SvgPause>
-                      )}
-                    </Btn>
-                  </Timer>
-                  <BurnedCal>
-                    Burned calories:{' '}
-                    <BurnedInformName>{burned}</BurnedInformName>
-                  </BurnedCal>
-                </Div>
-                <Ul>
-                  <Li>
-                    <Span>Name</Span>
-                    <P>{Body}</P>
-                  </Li>
-                  <Li>
-                    <Span>Target</Span>
-                    <P>{Target}</P>
-                  </Li>
-                  <Button type="button">Add to diary</Button>
-                </Ul>
-              </MadalContent>
-          </Modal>
-      </Transition>
-    </>
-  );
-};
-
+  }, [onClose]); 
+ 
+  const startPauseTimer = () => { 
+    setPause((prevPause) => !prevPause); 
+  }; 
+ 
+  const openModal = () => { 
+    onOpen(); 
+    onClose(); 
+  }; 
+ 
+  const namber = 180; 
+ 
+  return ( 
+    <> 
+      {/* <StyledModal> */} 
+      <Transition in={isOpen} timeout={350} unmountOnExit={true}> 
+        <Modale className="modal-wrapper" onClick={onWrapperClick}> 
+          <MadalContent className="modal-content"> 
+            <Svg onClick={() => onClose()}> 
+              <use href={`${sprite}#icon-x`}></use> 
+            </Svg> 
+            <Div> 
+              <Image></Image> 
+              <Timer> 
+                <CountdownCircleTimer 
+                  isPlaying={pause} 
+                  duration={namber} 
+                  colors={'#E6533C'} 
+                  colorsTime={0} 
+                  size={124} 
+                  strokeWidth={4} 
+                  trailColor={'rgba(239, 237, 232, 0.10)'} 
+                  isGrowing={true} 
+                  rotation={'counterclockwise'} 
+                  strokeLinecap={'square'} 
+                  children={({ remainingTime }) => ( 
+                    <div className="countdown-timer"> 
+                      {`${Math.floor(remainingTime / 60)}:${remainingTime % 60}`} 
+                    </div> 
+                  )} 
+                ></CountdownCircleTimer> 
+                <Btn type="button" onClick={startPauseTimer}> 
+                  {pause ? ( 
+                    <SvgPlay> 
+                      <use href={`${sprite}#icon-play`}></use> 
+                    </SvgPlay> 
+                  ) : ( 
+                    <SvgPause> 
+                      <use href={`${sprite}#icon-pause`}></use> 
+                    </SvgPause> 
+                  )} 
+                </Btn> 
+              </Timer> 
+              <BurnedCal> 
+                Burned calories: <BurnedInformName>{burned}</BurnedInformName> 
+              </BurnedCal> 
+            </Div> 
+            <Ul> 
+              <Li> 
+                <Span>Name</Span> 
+                <P>Air bike</P> 
+              </Li> 
+              <Li> 
+                <Span>Target</Span> 
+                <P>{Target}</P> 
+              </Li> 
+              <Li>
+                <Span>Body Part</Span> 
+                <P>{Body}</P> 
+              </Li> 
+              <Li> 
+                <Span>Equipment</Span> 
+                <P>Body weight</P> 
+              </Li> 
+              <Button type="button" onClick={openModal}> 
+                Add to diary 
+              </Button> 
+              {/* <AddExerciseSuccess 
+                isOpen={modalOpen} 
+                onClose={()=> setModalOpen(false)} 
+                calories={burned} 
+              /> */} 
+            </Ul> 
+          </MadalContent> 
+        </Modale> 
+      </Transition> 
+      {/* </StyledModal> */} 
+    </> 
+  ); 
+}; 
+ 
 export default BasicModalWindow;
