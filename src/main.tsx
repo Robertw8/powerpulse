41b21ba5b3c939883f0391d9180ca94974de36ea
Robertw8 <oniedgaf@gmail.com>
2024-01-30 00:26:11 +0200

    update router basename

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ import { persistor, store } from './redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/powerpulse/">
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <Global styles={GlobalStyles} />
         <PersistGate loading={null} persistor={persistor}>
